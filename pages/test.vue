3c0825d592cec4cc015a4e7b659882d80bd876e3
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2019-11-27 10:14:35 +0000

    プロパティの追加

--- pages/test.vue
+++ pages/test.vue
@@ -1,16 +1,17 @@
-
 <template>
-  <h1>{{ env_1 }}</h1>
+
 </template>
 
-<script>
-export default {
-  data() {
-    console.log(process.env.SONGJSONURL)
-    return {
-      env_1: process.env.SONGJSONURL,
-      // env_2: process.env.SONGJSONURL
-    }
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { DataTable }
+@Component({
+  head: {
+    title: '曲データ'
+  },
+  components: {
+    ScoreTable: () => import('~/components/DataTable.vue')
   }
-}
+})
 </script>
